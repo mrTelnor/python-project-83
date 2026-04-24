e1131c991c2c3da323390c133fc08b37f553090b
Никита Волков <mrtelnor@gmail.com>
2026-04-24 16:22:40 +0300

    Добавлен Jinja шаблон для главной страницы с Bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 
@@ -11,4 +11,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.get('/')
 def index():
-    return 'Page Analyzer'
+    return render_template('index.html')
